84c0d197c27490181872eb3ab82e8cabb0f6ced6
kamolbekb <118444680+kamolbekb@users.noreply.github.com>
2024-11-17 02:19:35 +0500

    ready

---
+++ src/N-Tier.Application/Helpers/JwtHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using N_Tier.DataAccess.Identity;
+
+namespace N_Tier.Application.Helpers;
+
+public static class JwtHelper
+{
+    public static string GenerateToken(ApplicationUser user, IConfiguration configuration)
+    {
+        ////var secretKey = configuration.GetValue<string>("JwtConfiguration:SecretKey");
+
+        ////var key = Encoding.ASCII.GetBytes(secretKey);
+
+        ////var tokenHandler = new JwtSecurityTokenHandler();
+
+        ////var tokenDescriptor = new SecurityTokenDescriptor
+        ////{
+        ////    Subject = new ClaimsIdentity(new[]
+        ////    {
+        ////        new Claim(ClaimTypes.NameIdentifier, user.Id),
+        ////        new Claim(ClaimTypes.Name, user.UserName),
+        ////        new Claim(ClaimTypes.Email, user.Email)
+        ////    }),
+        ////    Expires = DateTime.UtcNow.AddDays(7),
+        ////    SigningCredentials =
+        ////        new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        ////};
+
+        ////var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        ////return tokenHandler.WriteToken(token);
+
+        return "";
+    }
+}
